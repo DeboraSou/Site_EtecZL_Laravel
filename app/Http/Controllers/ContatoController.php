144c4f2b4b8be764e62e43cf1aebd8dede797aa8
DeboraSou <debora.d3v@gmail.com>
2024-04-27 23:53:02 -0300

    feat(controller): Adiciona no ContatoController o método submit().

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -5,10 +5,28 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ContatoController extends Controller {
+class ContatoController extends Controller
+{
     //Método Actions
-    public function principal() {
+    public function principal()
+    {
         // echo 'controller contato';
         return view('site.contato');
+    }
+
+    public function submit(Request $request)
+    {
+        // Validação das regras.
+        $rules = [
+            'fullname' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ];
+
+        // Validação do input.
+        $request->validate($rules);
+
+        return redirect()->back()->with('success', 'Mensagem enviada com sucesso!');
     }
 }
